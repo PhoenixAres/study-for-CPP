cad2cc2bcdf74a35a4abc3586aa2088621096ad5
PhoenixAres <phoenixhyy@163.com>
2020-12-10 20:10:26 +0800

    更新了1-10题

---
+++ 程序设计与算法（一）C++基础/006浮点型数据类型存储空间大小/答案.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float n;
+    double m;
+    cout << sizeof(n) << " " << sizeof(m) << endl;
+    return 0;
+}
---
+++ 程序设计与算法（一）C++基础/007对齐输出/答案.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    printf("%8d %8d %8d\n",a,b,c);
+    return 0;
+}
