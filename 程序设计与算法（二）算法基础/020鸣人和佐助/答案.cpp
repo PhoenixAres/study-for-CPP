87da875f37141ab6f5fe8a20005c55d78f781654
PhoenixAres <phoenixhyy@163.com>
2020-12-12 14:28:40 +0800

    更改了20题代码

--- 程序设计与算法（二）算法基础/020鸣人和佐助/答案.cpp
+++ 程序设计与算法（二）算法基础/020鸣人和佐助/答案.cpp
@@ -1,145 +1,122 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char mp[300][300];
-int mark[300][300];
-int minL[300][300][20];
-int m, n, t;
-int minLen;
-int tempLen;
-
-void dfs(int x, int y)
+struct ways
 {
-    if (tempLen >= minLen) return ;
-    if (tempLen >= minL[x][y][t]) return ;
-    minL[x][y][t] = tempLen;
-    if (mp[x][y] == '+')
-    {
-        minLen = min(minLen, tempLen);
-        return ;
-    }
-    if (x - 1 >= 0 && !mark[x-1][y])
-    {
-        mark[x-1][y] = 1;
-        if (mp[x-1][y] == '#')
-        {
-            if (t)
-            {
-                --t;
-                ++tempLen;
-                dfs(x - 1, y);
-                --tempLen;
-                ++t;
-            }
-        }
-        else
-        {
-            ++tempLen;
-            dfs(x - 1, y);
-            --tempLen;
-        }
-        mark[x-1][y] = 0;
-    }
-    if (x + 1 < m && !mark[x+1][y])
-    {
-        mark[x+1][y] = 1;
-        if (mp[x+1][y] == '#')
-        {
-            if (t)
-            {
-                --t;
-                ++tempLen;
-                dfs(x + 1, y);
-                --tempLen;
-                ++t;
-            }
-        }
-        else
-        {
-            ++tempLen;
-            dfs(x + 1, y);
-            --tempLen;
-        }
-        mark[x+1][y] = 0;
-    }
-    if (y + 1 < n && !mark[x][y+1])
-    {
-        mark[x][y+1] = 1;
-        if (mp[x][y+1] == '#')
-        {
-            if (t)
-            {
-                --t;
-                ++tempLen;
-                dfs(x, y + 1);
-                --tempLen;
-                ++t;
-            }
-        }
-        else
-        {
-            ++tempLen;
-            dfs(x, y + 1);
-            --tempLen;
-        }
-        mark[x][y+1] = 0;
-    }
-    if (y - 1 >= 0 && !mark[x][y-1])
-    {
-        mark[x][y-1] = 1;
-        if (mp[x][y-1] == '#')
-        {
-            if (t)
-            {
-                --t;
-                ++tempLen;
-                dfs(x, y - 1);
-                --tempLen;
-                ++t;
-            }
-        }
-        else
-        {
-            ++tempLen;
-            dfs(x, y - 1);
-            --tempLen;
-        }
-        mark[x][y-1] = 0;
-    }
-}
+    int x, y;
+    int t;
+    int steps;
+    ways(int xx, int yy, int tt, int s):x(xx), y(yy), t(tt), steps(s) {}
+};
+
+queue<ways> q;
+char s[300][300];
+int visited[300][300][20];
 
 int main()
 {
+//    freopen("D:\\c语言源程序\\测试数据.txt", "r", stdin);
+    int n, m, t;
     scanf("%d%d%d", &m, &n, &t);
     for (int i = 0; i < m; ++i)
-        scanf("%s", mp[i]);
-    bool judge = false;
+        scanf("%s", s[i]);
     int x, y;
     for (int i = 0; i < m; ++i)
-    {
         for (int j = 0; j < n; ++j)
-        {
-            if (mp[i][j] == '@')
+            if (s[i][j] == '@')
             {
                 x = i;
                 y = j;
-                judge = true;
-                break;
+            }
+    ways w(x, y, t, 0);
+    q.push(w);
+    memset(visited, 0, sizeof(visited));
+    visited[w.x][w.y][w.t] = 1;
+    while (!q.empty())
+    {
+        w = q.front();
+        if (s[w.x][w.y] == '+')
+        {
+            cout << w.steps << endl;
+            return 0;
+        }
+        else
+        {
+            if (w.x - 1 >= 0)
+            {
+                if (s[w.x-1][w.y] == '#')
+                {
+                    if (w.t && !visited[w.x-1][w.y][w.t-1])
+                    {
+                        ways ww(w.x-1, w.y, w.t-1, w.steps+1);
+                        q.push(ww);
+                        visited[w.x-1][w.y][w.t-1] = 1;
+                    }
+                }
+                else if (!visited[w.x-1][w.y][w.t])
+                {
+                    ways ww(w.x-1, w.y, w.t, w.steps+1);
+                    q.push(ww);
+                    visited[w.x-1][w.y][w.t] = 1;
+                }
+            }
+            if (w.y - 1 >= 0)
+            {
+                if (s[w.x][w.y-1] == '#')
+                {
+                    if (w.t && !visited[w.x][w.y-1][w.t-1])
+                    {
+                        ways ww(w.x, w.y-1, w.t-1, w.steps+1);
+                        q.push(ww);
+                        visited[w.x][w.y-1][w.t-1] = 1;
+                    }
+                }
+                else if (!visited[w.x][w.y-1][w.t])
+                {
+                    ways ww(w.x, w.y-1, w.t, w.steps+1);
+                    q.push(ww);
+                    visited[w.x][w.y-1][w.t] = 1;
+                }
+            }
+            if (w.x + 1 < m)
+            {
+                if (s[w.x+1][w.y] == '#')
+                {
+                    if (w.t && !visited[w.x+1][w.y][w.t-1])
+                    {
+                        ways ww(w.x+1, w.y, w.t-1, w.steps+1);
+                        q.push(ww);
+                        visited[w.x+1][w.y][w.t-1] = 1;
+                    }
+                }
+                else if (!visited[w.x+1][w.y][w.t])
+                {
+                    ways ww(w.x+1, w.y, w.t, w.steps+1);
+                    q.push(ww);
+                    visited[w.x+1][w.y][w.t] = 1;
+                }
+            }
+            if (w.y + 1 < n)
+            {
+                if (s[w.x][w.y+1] == '#')
+                {
+                    if (w.t && !visited[w.x][w.y+1][w.t-1])
+                    {
+                        ways ww(w.x, w.y+1, w.t-1, w.steps+1);
+                        q.push(ww);
+                        visited[w.x][w.y+1][w.t-1] = 1;
+                    }
+                }
+                else if (!visited[w.x][w.y+1][w.t])
+                {
+                    ways ww(w.x, w.y+1, w.t, w.steps+1);
+                    q.push(ww);
+                    visited[w.x][w.y+1][w.t] = 1;
+                }
             }
         }
-        if (judge) break;
+        q.pop();
     }
-
-    memset(mark, 0, sizeof(mark));
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < n; ++j)
-            for (int k = 0; k <= t; ++k)
-                minL[i][j][k] = (1 << 30);
-    tempLen = 0;
-    minLen = 1 << 30;
-    mark[x][y] = 1;
-    dfs(x, y);
-    if (minLen < (1 << 30)) printf("%d\n", minLen);
-    else printf("-1\n");
+    cout << "-1" << endl;
     return 0;
 }
